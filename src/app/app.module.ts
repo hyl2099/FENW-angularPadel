d86c2b3e8c20235b9a710b26e4fd90172a6b9fd1
hyl2099 <hyl2099@gmail.com>
2019-12-14 22:54:41 +0100

    add rest pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
+import { BannerComponent } from './banner/banner.component';
+import { IndexcontentComponent } from './indexcontent/indexcontent.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { InstalacionesComponent } from './instalaciones/instalaciones.component';
+import { ReservasComponent } from './reservas/reservas.component';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    BannerComponent,
+    IndexcontentComponent,
+    ServiciosComponent,
+    InstalacionesComponent,
+    ReservasComponent,
+    RegistroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
